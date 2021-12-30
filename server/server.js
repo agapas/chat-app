e410d81c7048a7faf8e3de12ff65a9e8c0871216
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2021-12-30 16:47:47 +0000

    Added client and server authentication with WebSockets

--- server/server.js
+++ server/server.js
@@ -20,9 +20,13 @@ app.use(cors(), express.json(), expressJwt({
 const typeDefs = fs.readFileSync('./schema.graphql', { encoding: 'utf8' });
 const resolvers = require('./resolvers');
 
-function context({ req }) {
+function context({ req, connection }) {
   if (req?.user) {
     return { userId: req.user.sub };
+  }
+  if (connection?.context?.accessToke) {
+    const decodedToken = jwt.verify(connection.context.accessToken, jwtSecret);
+    return { userId: decodedToken.sub };
   }
   return {};
 }
