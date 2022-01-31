be15ccfd43c866174312f99f390fbe3da20b2fd3
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2022-01-31 13:32:27 +0000

    Migrated to Apollo Client 3

--- client/src/hooks.js
+++ client/src/hooks.js
@@ -1,14 +1,20 @@
-import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
-import { messagesQuery, addMessageMutation, messageAddedSubscription } from './graphql/queries';
+import { useQuery, useMutation, useSubscription } from '@apollo/client';
+import {
+  messagesQuery, addMessageMutation, messageAddedSubscription
+} from './graphql/queries';
 
 export const useChatMessages = () => {
   const { data } = useQuery(messagesQuery);
   const messages = data ? data.messages : [];
+
   useSubscription(messageAddedSubscription, {
     onSubscriptionData: ({ client, subscriptionData }) => {
-      client.writeData({ data: {
-        messages: messages.concat(subscriptionData.data.messageAdded)
-      }});
+      client.writeQuery({
+        query: messagesQuery,
+        data: {
+          messages: messages.concat(subscriptionData.data.messageAdded)
+        }
+      });
     }
   });
   const [ addMessage ] = useMutation(addMessageMutation);
